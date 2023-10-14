8a590da323839fa82cf6044eb36491fee3f9a50e
Leon <80590169+Lantam@users.noreply.github.com>
2023-10-14 19:52:55 +0200

    modified:   docker-compose.yaml 	modified:   nginx/Dockerfile 	modified:   nginx/nginx.conf 	modified:   src/Dockerfile 	deleted:    src/Dockerfile.prod 	deleted:    src/entrypoint.prod.sh 	modified:   src/entrypoint.sh 	modified:   src/map/views.py 	deleted:    src/worker-entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-if [ "$DATABASE_NAME" = "postgres" ]
-then
-    echo "Waiting for postgres..."
+# if [ "$DATABASE_NAME" = "postgres" ]
+# then
+#     echo "Waiting for postgres..."
 
-    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-      sleep 0.1
-    done
+#     while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+#       sleep 0.1
+#     done
 
-    echo "PostgreSQL started"
-fi
-
-# python manage.py flush --no-input
-# python manage.py migrate
+#     echo "PostgreSQL started"
+# fi
 
 exec "$@"
--- src/map/views.py
+++ src/map/views.py
@@ -6,11 +6,6 @@ from .forms import SearchForm
 from .models import Api, Search
 
 
-# tries = 2
-# for i in range(tries):
-#     try:
-        # call_command('makemigrations')
-        # call_command('migrate')
 api_data = [
     [
         row.registration_number,
@@ -27,17 +22,6 @@ api_data = [
     ]
     for row in Api.objects.all()
 ]
-    # except ObjectDoesNotExist:
-        # if i == 0:
-        #     call_command('makemigrations')
-        #     call_command('migrate')
-        # if i == 1:
-        #     call_command('add_latest_api_data')
-    #     if i < tries - 1:
-    #         continue
-    #     else:
-    #         raise
-    # break
 
 
 def filter_input(m, api_data):
