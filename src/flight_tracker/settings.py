1165d8cd5b17ef4e60d236b71847c723493fc26f
Leon <80590169+Lantam@users.noreply.github.com>
2023-10-14 20:18:26 +0200

    modified:   docker-compose.yaml 	modified:   src/flight_tracker/settings.py 	renamed:    templates/index.html -> src/templates/index.html

--- src/flight_tracker/settings.py
+++ src/flight_tracker/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'flight_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [join(BASE_DIR, '..', 'templates')],
+        'DIRS': [join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
